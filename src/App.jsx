c69dfacde7465da856e8571e0a6b0591ab94c5fe
agusmar <agusyeah8@gmail.com>
2022-09-22 23:39:46 -0300

    Subida

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Navbar from './Content/Navbar';
+
+import './estilos/App.css';
+import Cards from './Components/Cards';
+
+
+
+
+
+const App =() =>{
+  return (
+    <>
+      <Navbar/>
+      <Cards/>
+    </>
+  );
+}
+
+
+export default App;
+
